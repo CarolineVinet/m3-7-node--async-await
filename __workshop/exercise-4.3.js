84d5d371eee0dcb39f1de26542e5515f962f3933
Caroline Vinet <carovinet@gmail.com>
2020-07-13 15:47:32 -0400

    exercise 4.3

--- __workshop/exercise-4.3.js
+++ __workshop/exercise-4.3.js
@@ -1,5 +1,14 @@
-const request = require('request-promise');
+const request = require("request-promise");
+
+let geekJoke = {
+  url: "https://geek-jokes.sameerkumar.website/api?format=json",
+  method: "GET",
+};
 
 const getGeekJoke = async () => {
-  // ha hahahh
+  const response = await request(geekJoke);
+  const data = JSON.parse(response);
+  return data.joke;
 };
+
+getGeekJoke().then((data) => console.log(data));
