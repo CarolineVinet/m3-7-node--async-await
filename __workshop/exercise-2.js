a474bc9f619c75cae75e5724e4d6964b481db8bc
Caroline Vinet <carovinet@gmail.com>
2020-07-13 12:10:29 -0400

    exercise22

--- __workshop/exercise-2.js
+++ __workshop/exercise-2.js
@@ -1,19 +1,22 @@
 // Exercise 2 - `getIssPosition`
 // -----------------------------
 
-const request = require('request-promise');
+const request = require("request-promise");
 
 const getIssPosition = async () => {
   try {
-    const response = await request(/* fill this in */);
-    const issLocation = JSON.parse(/* fill this in */);
-    return {
-      lat: /* fill this in */,
-      lng: /* fill this in */,
+    const response = await request("http://api.open-notify.org/iss-now.json");
+    const issLocation = JSON.parse(response);
+    const latitude = issLocation.iss_position.latitude;
+    const longitude = issLocation.iss_position.longitude;
+    const location = {
+      latitude,
+      longitude,
     };
+    console.log(location);
   } catch (err) {
-    console.log('Error: ', err);
+    console.log("Error: ", err);
   }
 };
 
-console.log(getIssPosition());
+getIssPosition();
